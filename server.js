866209915003dc67142d601cab2b470dacf6e7fd
Karthick <johnnykarthi22@gmail.com>
2023-10-10 12:27:50 +0530

    added cors

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 require('dotenv').config()
 const express = require('express');
 const app = express();
+const cors = require('cors');
 
 const PORT = process.env.PORT;
-
+app.use(cors());
 app.use(express.json());
 app.use((req,res,next)=>{
     console.log(req.url,req.method);
